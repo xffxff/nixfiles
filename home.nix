a80bc968c4316de1f8276a10c977d6267bdbd114
XFFXFF <1247714429@qq.com>
2022-11-18 18:26:44 +0800

    config fzf

--- home.nix
+++ home.nix
@@ -50,6 +50,12 @@
     ];
   };
 
+  # Fuzzy finder
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
