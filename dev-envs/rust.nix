5f60fed808e9b4d481fbf6a95cd590b647e48a9c
XFFXFF <1247714429@qq.com>
2022-11-27 01:23:06 +0000

    .

---
+++ dev-envs/rust.nix
@@ -0,0 +1,27 @@
+{ nixpkgs ? import <nixpkgs> { }}:
+
+let
+  rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [ (import rustOverlay) ];
+  };
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+    (pkgs.rust-bin.stable.latest.default.override {
+	  extensions = [ "rust-src" "cargo" "rustc" ];
+    })
+    pkgs.typos
+    pkgs.zola
+    pkgs.mdbook
+    pkgs.mdbook-mermaid
+    pkgs.mdbook-linkcheck
+    ];
+
+    shellHook = ''
+    zsh
+    '';
+
+    NIX_ENFORCE_PURITY = 0;
+    RUST_BACKTRACE = 1;
+  }
